c1046c5185afab7b6c2fba9f03765b1b8bf8c3bb
Mac Duy Khanh <macduykhanh2004@gmail.com>
2025-02-26 19:33:07 +0100

    update

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/macduy/Photo-Share/venv"
+set -gx VIRTUAL_ENV "/Users/macduy/Documents/GitHub/Software-Project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
